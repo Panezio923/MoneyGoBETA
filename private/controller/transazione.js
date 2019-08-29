573ce8208b4b2c1eac11c7f1f82aec2b9a8d1460
Panezio <sergiocomella@gmail.com>
2019-08-29 17:24:32 +0200

    17:23  29/08

--- private/controller/transazione.js
+++ private/controller/transazione.js
@@ -1,4 +1,5 @@
 const pool = require('./connessionedb');
+const crypto = require('crypto');
 
 function Transazione() {};
 
@@ -21,8 +22,8 @@ Transazione.prototype = {
 
         pool.query(sql, [d, causale, mittente, destinatario, importo, stato], function (err, result) {
             if(err) throw err;
-            if(result) callback(result);
-            else callback(null);
+            if(result) callback(true);
+            else callback(false);
         })
     },
 
@@ -58,6 +59,54 @@ Transazione.prototype = {
         })
     },
 
+    /*Funzione che crea una transazione in stato "sospesa" che viene accettata attraverso
+    * l'uso del token a cui è associata.
+    * @param {string} type - Se è un link di richiesta o di invio.
+    * @param {string} user - Colui che crea il link.
+    * @param {string} causale - La motivazione della creazione del link.
+    * @param {float} importo - L'importo della transazione.
+    * @param {function} callback - La funzione da eseguire una volta terminate le operazioni.
+    */
+    creaToken : function (type, user, causale, importo, callback) {
+        //Carico in token tutti i dati per la transazione e quando l'utente l'accetta effettuo la query su transazione ed invio/ricevi denaro. //TODO
+        //Il destinatario della richiesta che è il mittente/destinatario del denaro è iniziato a null. Dopo che logga diventa il suo nick e richiama la richiesta. //TODO
+        var token = crypto.createHmac('sha256', "random").update(user + Date.now()).digest('hex');
+        console.log(token);
+
+        let sql_token = "INSERT INTO token(token, id_transazione, data) VALUES(?,?,?);" ;
+
+        pool.getConnection(function (err, connection) {
+            if(err){
+                console.log(err);
+                callback(undefined, "CONNERR");
+            }
+            else{
+                connection.beginTransaction(function (err) {
+                    if (err) {
+                        console.log(err);
+                        callback("CONNERR");
+                        connection.rollback();
+                        connection.release();
+                    } else {
+                        this.newTransazione(causale, user, null, importo, "sospesa", function (esitoTransazione) {
+                            if(!esitoTransazione){
+                                callback(undefined, esitoTransazione);
+                                connection.rollback();
+                                connection.release();
+                            }
+                            else{
+                               console.log(esitoTransazione.id);
+                               console.log(esitoTransazione);
+                            }
+                        })
+                    }
+                })
+            }
+        })
+    }
+
 };
+
+
 
 module.exports = Transazione;
--- private/route/homeRoute.js
+++ private/route/homeRoute.js
@@ -10,6 +10,10 @@ const metodi = new Metodi();
 const conto = new Conto();
 const transazione = new Transazione();
 
+router.get('/', (req, res)=>{
+   req.session.conto.saldo_conto = calcolaSaldo(req.session.user.nickname);
+});
+
 router.get('/adminCards', (req,res,next) =>{
     let user = req.session.user;
     if(!user){
@@ -51,57 +55,140 @@ router.get('/gestioneProfilo', (req,res,next) =>{
     });
 });
 
-router.get('/ricaricaconto',(req,res,next) => {
-    if(!req.session.user){
-        res.redirect('/');
-        return
-    }
-    else res.render('ricaricaconto',{title:"MoneyGo", metodi : req.session.metodi, saldo_metodo : req.session.saldo_metodo});
-});
-
 router.get('/user_nickname', (req,res,next)=>{
     res.send(req.session.user.nickname);
+    res.end();
 });
 
-router.post('/inviaDenaro', (req, res, next)=>{
-    var mittente = req.session.user.nickname;
-    var destinatario = req.body.destinatario;
-    var metodo = req.body.metodo;
-    var importo = parseFloat(req.body.importo);
-    var causale = req.body.causale;
+/*
+ *Questo route verifica che il limite spesa non venga superato. Se così è
+ * restituisce il messaggio OVERLIMIT. Il parametro bypass verifica se l'utente vuole
+ * continuare la transazione anche se supera il limite di spesa.
+ */
+router.use('/inviaDenaro', (req, res, next)=>{
+    res.locals.importo = Number(req.body.importo);
+    var limite = req.session.conto.limite_spesa;
+    var bypass = req.body.bypass;
+    res.locals.mittente = req.session.user.nickname;
+    res.locals.metodo = req.body.metodo;
+    res.locals.destinatario = req.body.destinatario;
+    res.locals.causale = req.body.causale;
 
-    metodi.inviaDenaro(mittente, importo, destinatario, metodo, function (result) {
-        if (result) {
-            /*
-             * Aggiorno il valore del conto nella sessione solo quando viene selezionato quello di MoneyGo
-             * in quanto viene utilizzato nella home.
-             */
-            if (metodo === "MONEYGO") req.session.conto.saldo_conto = req.session.conto.saldo_conto - importo;
 
-            transazione.newTransazione(causale, mittente, destinatario, importo, "eseguita", function (newT) {
-                if (newT) {
-                    //Aggiorno le transazioni nella sessione
-                    transazione.recuperaTransazione(mittente, function (esito) {
-                        if (esito) {
-                            req.session.transazioni = esito;
-                            res.send("DONE");
-                            res.end();
-                        } else if (!esito) {
-                            res.send("FAULT");
-                            res.end();
-                        }
-                    })
-                } else if (!newT) {
-                    res.send("TRANERR");
-                    res.end();
-                }
-            })
-        } else if (!result) {
+    if (res.locals.importo >= limite && limite != null && limite !== 0 && bypass === "off") {
+        return res.send("OVERLIMIT");
+    }
+    next('route');
+});
+
+/*
+ * Questa route si occupa dell'invio di denaro eseguendo prima lo spostamento dei soldi
+ * e successivamente crea la transazione. Inoltre aggiorna i valori della sessione del saldo
+ * e delle transazioni.
+ */
+router.post('/inviaDenaro', (req,res,next)=>{
+    metodi.inviaDenaro(res.locals.mittente, res.locals.importo, res.locals.destinatario, res.locals.metodo, function (result) {
+        if(!result) {
             res.send("TOO");
             res.end();
+            return;
+        } else {
+            if (res.locals.metodo === "MONEYGO") req.session.conto.saldo_conto = (req.session.conto.saldo_conto - res.locals.importo).toFixed(2);
+            next('route');
         }
     })
 });
+
+router.post('/inviaDenaro', (req, res, next) =>{
+    transazione.newTransazione(res.locals.causale, res.locals.mittente, res.locals.destinatario, res.locals.importo, "eseguita", function (result) {
+        if(!result) return res.send("TRANERR");
+        else{
+            next('route');
+        }
+    })
+});
+
+router.post('/inviaDenaro', (req, res)=>{
+   transazione.recuperaTransazione(res.locals.mittente, function (result) {
+       if(!result) return res.send("FAULT");
+       else{
+           req.session.transazioni = result;
+           return res.send("DONE");
+       }
+   })
+});
+
+router.post("/richiediDenaro", (req, res, next)=>{
+   console.log("Creo la nuova transazione");
+   var mittente = req.body.reqmittente;
+   var destinatario = req.session.user.nickname;
+   var importo =  Number(req.body.importo);
+   console.log(importo);
+   var causale = req.body.causale;
+
+   /*
+    * In questa chiamata alla funzione inverto il mittente ed il destinatario
+    * in quanto il mittente è colui che invia denaro e non chi effettua la richiesta.
+    */
+   transazione.newTransazione(causale, mittente, destinatario, importo, "in attesa", function (esito) {
+       if(esito) res.send("DONE");
+       else res.send("FAULT");
+       res.end();
+   })
+});
+
+/*
+ * Questa route restituisce le transazioni in attesa per crearne una copia
+ * sul front-end
+ */
+router.get("/ricavaNotifiche", (req, res)=>{
+    //console.log(req.session.notifiche);
+   transazione.recuperaTransazioniInAttesa(req.session.user.nickname, function (esito) {
+       if(esito !== 0) {
+           req.session.notifiche = esito;
+           res.send(esito);
+       }
+       else res.send("NONE");
+
+   })
+});
+
+router.post("/accettaTransazione", (req, res)=>{
+    metodi.inviaDenaro(req.session.user.nickname, req.body.importo, req.body.destinatario, "MONEYGO", function (esito) {
+        if(!esito) res.send("ERROR");
+        else{
+            //console.log(req.body.id);
+            req.session.conto.saldo_conto = (req.session.conto.saldo_conto - req.body.importo).toFixed(2);
+            transazione.accettaTransazione(req.session.user.nickname, req.body.id, function (esitoDUE) {
+                if(!esitoDUE) res.send("ERROR");
+                else{
+                    res.send("DONE");
+                }
+                res.end();
+            })
+        }
+    })
+});
+
+router.post("/rifiutaTransazione", (req,res) =>{
+    transazione.rifiutaTransazione(req.session.user.nickname, req.body.id, function (esito) {
+        if(!esito) res.send("ERROR");
+        else res.send("DONE");
+        res.end();
+    })
+});
+
+router.post('/creaToken', (req, res, next)=>{
+
+
+});
+
+function calcolaSaldo(user){
+    conto.calcolaSaldo(user, function (esito) {
+        if(esito) return esito;
+        else throw SQLException;
+    })
+};
 
 
 module.exports = router;
